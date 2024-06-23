ffb7bbdf0864aefd9c3b4b79ace53f5283314a5c
Coffee-Expert <abhishekevingomesofficial@gmail.com>
2024-06-23 11:19:18 +0530

    inital commit

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+//2d- portfolio made by Kev.
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Hey there! I'm Kevin (Coffee-Expert), a tech enthusiast who loves playing with Java, Python, and Dart. 
+    Recently, I've been creating awesome stuff in JavaScript (Isn't this awesome?).
+    From full-stack apps to cloud solutions, I enjoy solving problems with code. 
+    Check out my projects on <a href="https://github.com/coffee-expert" target="_blank">Github</a>! to see what I've been up to! Happy coding!`,
+  "cs-degree": `This is my CS degree, proudly displayed on my wall. It’s more than just paper; it’s the foundation of my tech journey. Yes!, the theory was intense, but it set me up for success in the real world.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also make Subject and concept explanations on YouTube. Go sub to  (If you are an engineering student I'm sure you'll like the content.)!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+  <a href="https://www.youtube.com/@markiplier" target="_blank">Markiplier</a>, <a href="https://www.youtube.com/@GameTheory" target="_blank">The Game Theorists</a>, and <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a>!`,
+  bed: `This? Bed. This is where I sleep. Great ideas come to me when I'm lying in bed. If I don't write them down right away, I can't sleep because my mind won't stop thinking about them..`,
+  resume: `This is my desk and on it is my resume. (Upload Pending) Contact me at abhishekevingomes@gmail.com if you have any interesting job opportunities.!`,
+  projects: `Info about this portfolio: It's made with the Kaboom.js library, which is a library for making games in JavaScript. Text is rendered with HTML/CSS, so the textbox you're currently reading is not rendered within the canvas. In case you didn't see my <a href="https://github.com/coffee-expert" target="_blank">Github</a>, now's a great time.`,
+  library: `There are a lot of books on my shelves. There is even one in French (I'm learning French btw. C'est une belle vie).
+  I've probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Kevin's portfolio, just close this tab.`,
+};
---
+++ src/kaboomCtx.js
@@ -0,0 +1,10 @@
+//2d- portfolio made by Kev.
+import kaboom from "kaboom";
+import { scaleFactor } from "./constants";
+
+export const k = kaboom({
+  global: false,
+  touchToMouse: true,
+  canvas: document.getElementById("game"),
+  debug: false,
+});
